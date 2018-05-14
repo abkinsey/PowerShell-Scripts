0c26ac619c3d089a3a33d60458fdc9494347c36b
Andrew Kinsey <abkinsey@gmail.com>
2018-05-14 11:55:18 -0400

    Add Supervisor to a List of AD Accounts

---
+++ add_supervisor.ps1
@@ -0,0 +1,28 @@
+﻿# #############################################################################
+# Company Name - SCRIPT - POWERSHELL
+# NAME: add_supervisor.ps1
+# 
+# AUTHOR:  Andrew Kinsey
+# DATE:  2018/04/25
+# EMAIL: 
+# 
+# COMMENT:  This script will pull a list of users and assign the supervisor to
+#            user account.
+#
+# VERSION HISTORY
+# 1.0 2018.04.25 Initial Version.
+# 
+#
+# TO ADD
+# -Add a Function to ...
+# -Fix the...
+# #############################################################################
+
+Import-Module ActiveDirectory
+$Users = Import-csv 'C:\Prod_Scripts\input\Users to Import.csv'
+foreach ($User in  $Users)
+{
+Set-ADUser $User.SamAccountName -Manager $User.Newmanager
+}
+
+

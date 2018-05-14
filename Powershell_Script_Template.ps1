dbaad333d2635ec02ac8e394da673ea2223fc703
Andrew Kinsey <abkinsey@gmail.com>
2018-05-14 12:43:41 -0400

    PowerShell Script Template

---
+++ Powershell_Script_Template.ps1
@@ -0,0 +1,19 @@
+﻿# #############################################################################
+# Company Name - SCRIPT - POWERSHELL
+# NAME: script.ps1
+# 
+# AUTHOR:  
+# DATE:  2016/03/14
+# EMAIL: 
+# 
+# COMMENT:  This script will....
+#
+# VERSION HISTORY
+# 1.0 2011.05.25 Initial Version.
+# 1.1 2011.06.14 Upgrade with...
+#
+# TO ADD
+# -Add a Function to ...
+# -Fix the...
+# #############################################################################
+

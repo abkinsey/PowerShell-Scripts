63c98f96eb904c2bfe7e20603d4eebca9f5b161d
Andrew Kinsey <abkinsey@gmail.com>
2018-10-30 09:28:37 -0400

    Auto Print Documents From a Folder

---
+++ AutoPrint.ps1
@@ -0,0 +1,25 @@
+﻿# ###############################################################################
+#  - SCRIPT - POWERSHELL
+# NAME: AutoPrint.ps1
+# 
+# AUTHOR:  Andrew Kinsey
+# DATE:  2018/10/30
+# EMAIL: 
+# 
+# COMMENT:  This script will automatically print all documents from a particular
+#           folder.
+#
+# VERSION HISTORY
+# 1.0 2018.10.30 Initial Version.
+# 
+# ##############################################################################
+
+$orders = "C:\autoprint\"
+
+
+$items = Get-ChildItem -Path "c:\autoprint\" -Filter "*.*"
+
+
+foreach ($item in $items) { Start-Process –FilePath $orders\$item –Verb Print -PassThru | %{sleep 5;$_} | kill     }
+
+

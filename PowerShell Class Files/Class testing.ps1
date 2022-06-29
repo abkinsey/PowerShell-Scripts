ca55616b74c1f81868444f256e2b2317f8cf5028
abkinsey <abkinsey@gmail.com>
2022-06-28 22:04:04 -0400

    Updated

--- PowerShell Class Files/Class testing.ps1
+++ PowerShell Class Files/Class testing.ps1
@@ -1,13 +1,22 @@
 $logs = Get-EventLog -List | Select-Object LogDisplayName |
 Out-GridView -Title "Select one of more logs" -OutputMode Multiple
 
-$count = 1, 10, 25, 50 | Out-GridView  -Title "How Many Entries" -OutputMode Single
+
+$count = 1, 10, 25, 50 | Out-GridView  -Title "How Many Entries" -OutputMode Single
+
+
 $printer = Get-Printer | Select-Object Name | Out-GridView -Title "Select a Printer" -OutputMode Single
 
-$logs | ForEach-Object {
-    $_.LogDisplayName
-    Get-EventLog -LogName $_.LogDisplayName -Newest $count -EntryType  Error |
-    Select-Object     Timegenerated, Source, Message | Format-List |
-    Out-Printer $printer.Name
-}
+$logs
+$count
+$printer
 
+
+
+
+$logs | ForEach-Object {
+    $_.LogDisplayName
+    Get-EventLog -LogName $_.LogDisplayName -Newest $count -EntryType  Error |
+    Select-Object     Timegenerated, Source, Message | Format-List |
+    Out-Printer $printer.Name
+}

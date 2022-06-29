a0605cac09c93195e9f13c9908b0ce04c97c401a
abkinsey <abkinsey@gmail.com>
2022-06-29 17:34:08 -0400

    Testing

--- PowerShell Class Files/Class testing.ps1
+++ PowerShell Class Files/Class testing.ps1
@@ -1,8 +1,12 @@
+$PSStyle.OutputRendering = 'Host'
+
+
 help Tee-Object
 #Gets help on the Tee-Object command.  Tee-Object will save the command
 # output in a file or variable and also sent it down the pipeline
 
 Get-Process | Sort-Object WS -Descending | Select-Object -First 10 | Tee-Object -FilePath C:\PowerShellTraining\proc.txt
+
 
 
 

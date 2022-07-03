3046d990769ae3b0d4eb8e80ec49f90371bcd37c
abkinsey <abkinsey@gmail.com>
2022-07-03 18:28:19 -0400

    new module

--- PowerShell Class Files/Class testing.ps1
+++ PowerShell Class Files/Class testing.ps1
@@ -1,40 +1,15 @@
+Get-Vegetable
 
+Get-Vegetable | Select-Object Name -Unique
 
-#***** Begin Script Block ******
-Get-Process | Sort-Object cpu -Descending |
-Select-Object -First 10 -Property Id, Name, CPU, StartTime,
-@{Name = "Runtime" ;Expression={(Get-Date)-$_.StartTime}}
-#***** End Script BLock *******
+Get-Vegetable | Select-Object Name -Unique | Out-File C:\PowerShellTraining\veggies.txt
 
-Get-Service bits  | Select-Object *
+Get-Vegetable | Select-Object -ExpandProperty Name -Unique
 
-Get-Service -machinename akinsey-laptop | Select-Object DisplayName, Status, @{Name="ComputerName";Expression={$_.machinename}}
+Get-Vegetable | Select-Object -ExpandProperty Name -Unique | Out-File C:\PowerShellTraining\veggies.txt
 
-#********* This will only work in older verions of PowerShell and not v7 ***************
-Get-Service -ComputerName ps-srvr01, Win10-PC1 | Select-Object DisplayName, Status,
-@{name="ComputerName";Expression={$_.machinename}} | Out-GridView -Title "Services"
-#*****************************************************************************************
+help Get-Vegetable -Parameter Name
 
-#****** Powershell 7 way to accomplish this on a remote machine *******
-New-PSSession -ComputerName ps-srvr01
-Enter-PSSession 1
-
-Get-Service -ComputerName ps-srvr01 | Select-Object DisplayName, Status,
-@{name="ComputerName";Expression={$_.machinename}} | Out-File c:\files\test2.txt
-#******************************************************************************
-#************* Script Bloak Begin ********************
-dir -Path C:\PowerShellTraining -file -Recurse | Select-Object Name,
-@{Name="sizeKB";Expression={[math]::round($_.length/1kb,2)}},
-@{Name="Modified";Expression={$_.lastwritetime}},
-@{Name="ModifiedAge";Expression={(Get-Date) - $_.lastwritetime}},
-@{name="Path";Expression={$_.fullname}},
-@{Name="ComputerName";Expression={$env:COMPUTERNAME}}
-#************* Script Bloak End ********************
-
-
-
-
-
-
+Get-Content C:\PowerShellTraining\veggies.txt | Where-Object {$_ -like "c*"} |Get-Vegetable
 
 

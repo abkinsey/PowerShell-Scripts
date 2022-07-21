56b2261e400031edac2f6585da24ade2ebc6ba7c
abkinsey <abkinsey@gmail.com>
2022-07-20 21:48:55 -0400

    Training Script Updates

--- PowerShell Class Files/Class testing.ps1
+++ PowerShell Class Files/Class testing.ps1
@@ -1,4 +1,6 @@
-help Format-Table
+help Format-Table | more
+#formats the output as a table
+
 
 Get-ChildItem C:\PowerShellTraining\*.dat | Format-Table -Property Fullname, length, lastwritetime
 
@@ -7,3 +9,9 @@ Get-ChildItem C:\PowerShellTraining\*.dat | format-table -property Fullname,Name
 @{Name="Age(Days)";Expression = { (Get-Date)-$_.lastwritetime | Select-Object -ExpandProperty TotalDays }}
 
 Get-Vegetable | Format-Table -GroupBy color 
+
+Get-Vegetable | Sort-Object Color | Format-Table -GroupBy color -property UPC, Count, Name, ISroot
+
+dir C:\PowerShellTraining -File -Recurse | Group-Object Extension | Select-Object Count, Name,
+@{Name="TotalKB";Expression={$_.Group | Measure-Object length -Sum | Select-Object -ExpandProperty sum}} |
+Format-Table -GroupBy Name -Property Count, TotalKB

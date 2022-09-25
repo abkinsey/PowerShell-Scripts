ba7c2844204f5ba7d6ed8c9a71d94875ae32472a
Andrew Kinsey <abkinsey@gmail.com>
2022-09-25 08:26:36 -0400

    Rename Files Sequentially

---
+++ Misc Scripts/File_Rename_Script.ps1
@@ -0,0 +1,17 @@
+﻿$path = Read-Host "Directory Path?"
+
+$startnumber = Read-Host "Starting Number?"
+
+$prefix = Read-Host "prefix?"
+
+$suffix = Read-Host "Suffix?"
+
+
+$path
+$startnumber
+$prefix
+$suffix
+
+Get-ChildItem $path | %{Rename-Item $_ -NewName ($prefix + ‘- 0{0} by ’ + $suffix -f $startnumber++)}
+
+# Get-ChildItem 'C:\scripts\File Rename Scripts\' | %{Rename-Item $_ -NewName (‘Urban Legends - 0{0} by Brandon Thomas.cbr’ -f $nr++)}

528d13f8fb582713f8eb40445e69593cb55b0d57
Andrew Kinsey <abkinsey@gmail.com>
2022-09-26 09:35:37 -0400

    Working Version

--- Misc Scripts/File_Rename_Script.ps1
+++ Misc Scripts/File_Rename_Script.ps1
@@ -1,17 +1,15 @@
-﻿$path = Read-Host "Directory Path?"
+﻿#$path = Read-Host "Directory Path?"
 
-$startnumber = Read-Host "Starting Number?"
+[int] $startnumber = Read-Host "Starting Number?"
 
 $prefix = Read-Host "prefix?"
 
 $suffix = Read-Host "Suffix?"
 
+$author = Read-Host "Author?"
 
-$path
-$startnumber
-$prefix
-$suffix
+$type = Read-Host "Type of Comic (TP, HC, Omnibus)?"
 
-Get-ChildItem $path | %{Rename-Item $_ -NewName ($prefix + ‘- 0{0} by ’ + $suffix -f $startnumber++)}
 
-# Get-ChildItem 'C:\scripts\File Rename Scripts\' | %{Rename-Item $_ -NewName (‘Urban Legends - 0{0} by Brandon Thomas.cbr’ -f $nr++)}
+Get-ChildItem 'C:\Users\abkin\Desktop\Rename\' | %{Rename-Item $_ -NewName ($prefix + ‘ - 0{0} by ’ -f $startnumber++ + $author + ‘ - ’ + $type +$_.extension)}
+

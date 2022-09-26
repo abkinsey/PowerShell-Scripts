5a63714bb432cdee89a1058cc6fe40ea4cae9455
Andrew Kinsey <abkinsey@gmail.com>
2022-09-26 16:53:16 -0400

    Began Adding Comments

--- Misc Scripts/Comicbook_Rename_Script.ps1
+++ Misc Scripts/Comicbook_Rename_Script.ps1
@@ -1,4 +1,33 @@
-﻿
+﻿# #############################################################################
+# Company Name - SCRIPT - POWERSHELL
+# NAME: Comicbook_Rename_Script.ps1
+# 
+# AUTHOR:  Andrew Kinsey
+# DATE:  2022/09/26
+# EMAIL: 
+# 
+# COMMENT:  This script will rename all files in a directory with sequential numbering. 
+#           The following information will be requested prior to renaming the files.
+#               1. Path - The directory containing the files to be renamed (NOTE: It will rename all files in the path specified)
+#               2. Starting Number - It will use this as the first number when renaming the files
+#               3. Prefix - filename to use prior to the number
+#               4. Author - Writers name (shows after the number)
+#               5. Type - Type of comic (Single, Trade, Hardcover, Omnibus)
+#
+#               The file name will look like this after the rename: Amazing Spider-Man 02 by Zeb Wells.cbr
+#
+# VERSION HISTORY
+# 1.0 2018.04.25 Initial Version.
+# 
+#
+# TO ADD
+# -Add a Function to ...
+# -Fix the...
+# #############################################################################
+
+
+$path = Read-Host "Enter Path to working directory"
+
 [int] $startnumber = Read-Host "Starting Number?"
 
 $prefix = Read-Host "prefix?"
@@ -21,7 +50,7 @@ if ($choice -eq '1') { $type = "HC" }
 if ($choice -eq '1' -or $choice -eq '2' -or $choice -eq '3') {
     Write-Host "You picked Choice 1, 2 or 3!"
     $Type
-    Get-ChildItem 'C:\Users\abkin\Desktop\Rename\' | %{Rename-Item $_ -NewName ($prefix + ‘ - 0{0} by ’ -f $startnumber++ + $author + ‘ - ’ + $type +$_.extension)}
+    Get-ChildItem $path | %{Rename-Item $_ -NewName ($prefix + ‘ - 0{0} by ’ -f $startnumber++ + $author + ‘ - ’ + $type +$_.extension)}
 }
 else {
     Write-Host "You chose poorly!!"

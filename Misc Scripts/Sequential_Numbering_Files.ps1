827ce929cc0433f957f29680deb0bdaba871db83
Andrew Kinsey <abkinsey@gmail.com>
2022-10-16 16:28:44 -0400

    Header info Added

--- Misc Scripts/Sequential_Numbering_Files.ps1
+++ Misc Scripts/Sequential_Numbering_Files.ps1
@@ -3,13 +3,13 @@
 # NAME: Sequential_Numbering_Files.ps1
 # 
 # AUTHOR:  
-# DATE:  2016/03/14
+# DATE:  2022/10/16
 # EMAIL: 
 # 
 # COMMENT:  This script will....
 #
 # VERSION HISTORY
-# 1.0 2011.05.25 Initial Version.
+# 1.0 2022.10.16 Initial Version.
 # 1.1 2011.06.14 Upgrade with...
 #
 # TO ADD

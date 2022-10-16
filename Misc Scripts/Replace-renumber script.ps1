6787e3514afeda92fbb778f2f3b99b68cbc72397
Andrew Kinsey <abkinsey@gmail.com>
2022-10-16 16:20:54 -0400

    Rename / Renumber Script

---
+++ Misc Scripts/Replace-renumber script.ps1
@@ -0,0 +1,22 @@
+﻿# #############################################################################
+# Company Name - SCRIPT - POWERSHELL
+# NAME: Replace-Renumber_Files.ps1
+# 
+# AUTHOR:  Andrew Kinsey
+# DATE:  2022/10/16
+# EMAIL: 
+# 
+# COMMENT:  This script will....
+#
+# VERSION HISTORY
+# 1.0 2022.10.16 Initial Version.
+# 1.1 2011.06.14 Upgrade with...
+#
+# TO ADD
+# -Add a Function to ...
+# -Fix the...
+# #############################################################################
+
+$nr = 1
+
+Get-ChildItem  "c:\temp\*.*" | %{Rename-Item $_ -NewName (‘0{0}’ -f $nr++ + $($_.Name.Substring($_.Name.IndexOf('-'))))}

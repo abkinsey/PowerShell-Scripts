5d659072ad05d38d8ab5ab83d9330a8044786bd5
Andrew Kinsey <abkinsey@gmail.com>
2022-10-22 21:25:36 -0400

    scripts

--- Misc Scripts/Add_String_To_End_of_Filename.ps1
+++ Misc Scripts/Add_String_To_End_of_Filename.ps1
@@ -1,4 +1,7 @@
 #Get-ChildItem -Path \\Mac\Home\Desktop\Test | Rename-Item -NewName { $_.Name -replace '\.','-b.' }
 
-Get-ChildItem -Path \\Mac\Home\Desktop\Test | Rename-Item -NewName { $_.Name -replace '\.',' James Bond.' }
+#Get-ChildItem -Path \\Mac\Home\Desktop\Test | Rename-Item -NewName { $_.Name -replace '\.',' James Bond.' }
 
+Get-ChildItem \\Mac\Home\Desktop\Test | select name |export-csv \\Mac\Home\Desktop\test.csv
+
+Import-Csv \\Mac\Home\Desktop\test.csv | 
---
+++ Misc Scripts/Scooby_Rename.ps1
@@ -0,0 +1,15 @@
+#Define path to .csv file
+$ImportFile = "\\Mac\Home\Desktop\Scooby-Doo Rename.xlsx"
+
+#Import Users from $to be moved into an array called $userobjects 
+$userobjects = Import-CSV $ImportFile
+
+
+ForEach ( $userobject in $userobjects ) {
+ #Assign the content to variables
+ $FileUsername = $userobject.username
+ $FileDepartment = $userobject.department
+ $FileManager = $userobject.manager
+ #$FileAddress = $userobject.address
+ #$FilePostalCode = $userobject.postalcode
+ #$FilePhoneNumber = $userobject.phone

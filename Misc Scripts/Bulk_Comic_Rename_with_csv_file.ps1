4876c3afddd4bbfc6414527b386e84ec9b8aa416
Andrew Kinsey <abkinsey@gmail.com>
2022-11-22 09:02:01 -0500

    changes file parameters

--- Misc Scripts/Bulk_Comic_Rename_with_csv_file.ps1
+++ Misc Scripts/Bulk_Comic_Rename_with_csv_file.ps1
@@ -29,8 +29,8 @@
 # -Fix the...
 # #############################################################################
 
-$folder="C:\Temp2\Spawn" #target folder containing files
-$csv="c:\temp2\spawn_rename.csv" #path to CSV file
+$folder="C:\Temp\rename" #target folder containing files
+$csv="c:\temp\TMNT Amazing Adventures.csv" #path to CSV file
 
 cd ($folder); import-csv ($csv)| foreach {rename-item -path $_.path -newname $_.filename} 
 

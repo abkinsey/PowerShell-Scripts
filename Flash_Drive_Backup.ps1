f0512ba78c37c54e518188490365f94a8df6dec6
Andrew Kinsey <abkinsey@gmail.com>
2023-02-15 10:00:14 -0500

    Updated Script

--- Flash_Drive_Backup.ps1
+++ Flash_Drive_Backup.ps1
@@ -1,22 +1,23 @@
-<#
+# Prompt user for username and flash drive letter
+$username = Read-Host "Enter username"
+$driveLetter = Read-Host "Enter flash drive letter (e.g. E:)"
 
-$name = Read-Host "Enter a name for the directory"
-$src = Read-Host "Enter the source path (e.g. F:\)"
-$dst = "C:\Users\andrew\Dropbox\Flash Drive Backups\$name"
+# Create a directory with the username
+$directoryPath = "C:\Users\abkin\Dropbox\Flash Drive Backups\$username"
+New-Item -ItemType Directory -Path $directoryPath
 
-New-Item -ItemType Directory -Path $dst
-Copy-Item "$src\*" -Destination $dst -Recurse
+# Copy contents of flash drive to the directory, maintaining directory structure
+$sourcePath = "$driveLetter\*"
+$destinationPath = "$directoryPath\"
+$files = Get-ChildItem -Path $sourcePath -Recurse
 
-#>
-
-
-$name = Read-Host "Enter a name for the directory"
-$src = Read-Host "Enter the source path (e.g. F:\)"
-$dst = "C:\Users\andrew\Dropbox\Flash Drive Backups\$name"
-
-New-Item -ItemType Directory -Path $dst
-
-Get-ChildItem $src -Recurse | ForEach-Object {
-    Write-Output "Copying $($_.FullName) to $dst\$($_.Name)"
-    Copy-Item $_.FullName -Destination "$dst\$($_.Name)"
+# Use a progress bar to show file copy status
+$totalFiles = $files.Count
+$currentFile = 0
+foreach ($file in $files) {
+    $currentFile++
+    Write-Progress -Activity "Copying files" -Status "Copying $($file.Name) ($currentFile of $totalFiles)" -PercentComplete ($currentFile / $totalFiles * 100)
+    Copy-Item $file.FullName -Destination $destinationPath -Recurse -Force
 }
+
+Write-Host "File copy complete!"

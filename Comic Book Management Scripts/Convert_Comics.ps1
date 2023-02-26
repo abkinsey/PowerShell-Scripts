d9debd35512585e0d92d3ea68e43e137ea94525e
Andrew Kinsey <abkinsey@gmail.com>
2023-02-26 14:31:05 -0500

    Udated script

--- Comic Book Management Scripts/Convert_Comics.ps1
+++ Comic Book Management Scripts/Convert_Comics.ps1
@@ -17,13 +17,19 @@
 # -Fix the...
 # #############################################################################
 
-
+#Renames all .cbz files in the current directory to *.zip
 Get-ChildItem | rename-item -NewName {$_.name -replace ".cbz",".zip"}
+
+#List out all of the .zip files to be converted
+Get-ChildItem *.zip
+
+#Starts WinRar application to start the conversion
 Start-Process "C:\Program Files (x86)\WinRAR\WinRAR.exe"
 
-Get-ChildItem *.zip
+#Pauses unil the files are converted with WinRar
 Pause
 
+#Renames the converted .rar files to the .cbr cominc book reader format
 Get-ChildItem | rename-item -NewName {$_.name -replace ".rar",".cbr"}
 
 Pause
